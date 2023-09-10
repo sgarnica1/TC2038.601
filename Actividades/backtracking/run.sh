c4755063950ac3c82b6c032a8f27450d14d565d7
salgue441 <carlossalguero441@gmail.com>
2023-09-10 13:40:50 -0600

    final changes

--- Actividades/backtracking/run.sh
+++ Actividades/backtracking/run.sh
@@ -1,4 +1,12 @@
 cd src
 
-g++ main.cpp -o app
-./app
+# Compile and run the maze program
+g++ -std=c++20 main.cpp maze_solver/maze_solver.cpp -o maze_program
+./maze_program
+
+
+# Remove the maze program
+rm maze_program
+
+# Move back to the root directory
+cd ..
