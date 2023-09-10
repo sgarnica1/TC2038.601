1aaaa958c6634c2c7069fae16869b85fe53a4e91
Carlos Salguero <75867326+salgue441@users.noreply.github.com>
2023-09-10 13:36:55 -0600

    Update maze_solver.cpp

--- Actividades/backtracking/src/maze_solver/maze_solver.cpp
+++ Actividades/backtracking/src/maze_solver/maze_solver.cpp
@@ -10,6 +10,8 @@
  * @copyright Copyright (c) 2023
  *
  */
+// C++ Standard Library
+#include <array>
 
 // Project files
 #include "maze_solver.h"
@@ -22,10 +24,12 @@
  * @param rows Number of rows of the maze
  * @param cols Number of columns of the maze
  */
-MazeSolver::MazeSolver(const std::vector<std::vector<int>> &maze,
-                       const int &rows, const int &cols)
-    : m_maze{maze}, m_rows{rows}, m_cols{cols}
+MazeSolver::MazeSolver(const Maze &maze) : m_maze{maze}
 {
+    m_rows = static_cast<int>(m_maze.size());
+    m_cols = static_cast<int>(m_maze[0].size());
+
+    m_visited.resize(m_rows, std::vector<std::int8_t>(m_cols, 0));
 }
 
 // Methods (public)
@@ -34,33 +38,32 @@ MazeSolver::MazeSolver(const std::vector<std::vector<int>> &maze,
  * Solve the maze
  * @return true If the maze was solved
  * @return false If the maze couldn't be solved
- * @time_complexity O(2^(n^2))
- * @space_complexity O(n^2)
+ *
  */
-bool MazeSolver::solve_maze()
+bool MazeSolver::solve_maze() const
 {
-    if (solve_maze_until(0, 0))
-        return true;
-
-    return false;
+    return solve_maze_until(0, 0);
 }
 
 /**
  * @brief
  * Convert the maze to a string
  * @return std::string String representation of the maze
- * @time_complexity O(n^2)
- * @space_complexity O(n^2)
  */
 std::string MazeSolver::to_string() const noexcept
 {
+    Maze solutionMaze = m_maze;
+
+    for (const auto &[key, value] : m_solutions)
+        solutionMaze[value.first][value.second] = 2;
+
     std::string maze_string{""};
     maze_string.reserve(m_rows * m_cols);
 
     for (int i{}; i < m_rows; i++)
     {
         for (int j{}; j < m_cols; j++)
-            maze_string += m_solutions.count(i * m_cols + j) ? "1 " : "0 ";
+            maze_string += (solutionMaze[i][j] == 2) ? "1 " : "0 ";
 
         maze_string += "\n";
     }
@@ -76,48 +79,50 @@ std::string MazeSolver::to_string() const noexcept
  * @param col Column of the next position
  * @return true If the next position is safe
  * @return false If the next position is not safe
- * @time_complexity O(1)
- * @space_complexity O(1)
  */
-bool MazeSolver::is_safe(const int &row, const int &col)
+inline bool MazeSolver::is_safe(const int &row, const int &col) const noexcept
 {
     return (row >= 0 && row < m_rows && col >= 0 && col < m_cols &&
-            m_maze[row][col] == 1);
+            m_maze[row][col] == 1 && !m_visited[row][col]);
 }
 
 /**
  * @brief
- * Solve the maze until the end is reached
+ * Solve the maze until the end is reached. Uses directional
+ * recursion to solve the maze.
  * @param row Row of the current position
  * @param col Column of the current position
  * @return True if the maze was solved
  * @return False if the maze couldn't be solved
- * @time_complexity O(2^(n^2)) as the worst case is when the maze is solved by
- *                             traversing the whole maze
- * @space_complexity O(n^2) as the worst case is when the maze is solved by
- *                         traversing the whole maze
  */
 bool MazeSolver::solve_maze_until(const int &row,
-                                  const int &col)
+                                  const int &col) const noexcept
 {
     if (row == m_rows - 1 && col == m_cols - 1 &&
         m_maze[row][col] == 1)
     {
-        m_solutions[row * m_cols + col] = {row, col};
+        m_solutions.emplace(row * m_cols + col,
+                            std::make_pair(row, col));
+
         return true;
     }
 
     if (is_safe(row, col))
     {
-        m_solutions[row * m_cols + col] = {row, col};
+        m_solutions.emplace(row * m_cols + col,
+                            std::make_pair(row, col));
 
-        if (solve_maze_until(row + 1, col))
-            return true;
+        m_visited[row][col] = 1;
 
-        if (solve_maze_until(row, col + 1))
-            return true;
+        static const std::array<std::pair<int, int>, 2>
+            directions = {{{1, 0}, {0, 1}}};
 
-        m_solutions.erase(m_solutions.size() - 1);
+        for (const auto &[dRow, dCol] : directions)
+            if (solve_maze_until(row + dRow, col + dCol))
+                return true;
+
+        m_solutions.erase(row * m_cols + col);
+        m_visited[row][col] = 0;
     }
 
     return false;
