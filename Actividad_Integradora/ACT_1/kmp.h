45f86a21cf3c56f50cfb4839877e409873b7eb6d
Sergio Garnica <sgarnica1902@gmail.com>
2023-10-01 10:34:11 -0600

    Add KMP algorithm for pattern searching in text

---
+++ Actividad_Integradora/ACT_1/kmp.h
@@ -0,0 +1,92 @@
+/**
+ * @file kmp.h
+ * @brief Knuth-Morris-Pratt algorithm for string matching (KMP)
+ * @details
+ * The Knuth-Morris-Pratt algorithm is a string matching algorithm that finds the
+ * occurrence of a pattern in a text. It uses the information about the previous
+ * matches to determine where the next match could begin, thus improving the
+ * overall performance of the algorithm.
+*/
+
+#ifndef KMP_H
+#define KMP_H
+
+#include <bits/stdc++.h>
+
+void computeLPSArray(char *pat, int M, int *lps);
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char *pat, char *txt)
+{
+  int M = strlen(pat); // M = length of the pattern
+  int N = strlen(txt); // N = length of the text 
+  int lps[M];
+
+  // Calculate lps[] that will hold the longest prefix suffix
+  computeLPSArray(pat, M, lps);
+
+  int txt_index = 0; 
+  int pat_index = 0;
+  while ((N - txt_index) >= (M - pat_index))
+  {
+    if (pat[pat_index] == txt[txt_index])
+    {
+      pat_index++;
+      txt_index++;
+    }
+
+    if (pat_index == M)
+    {
+      std::cout << "Found pattern at index " << txt_index - pat_index << std::endl;
+      pat_index = lps[pat_index - 1];
+    }
+
+    else if (txt_index < N && pat[pat_index] != txt[txt_index])
+    {
+      if (pat_index != 0)
+        pat_index = lps[pat_index - 1];
+      else
+        txt_index = txt_index + 1;
+    }
+  }
+}
+
+/**
+ * @brief
+ * Computes the longest prefix suffix for the pattern
+ * @param pat - Pattern to be searched
+ * @param M - Length of the pattern
+ * @param lps - Array to store the longest prefix suffix
+ * @return void
+ * @Time Complexity - O(M)
+ * @Space Complexity - O(M)
+ */
+void computeLPSArray(char *pat, int M, int *lps)
+{
+  int len = 0; // length of the previous longest prefix suffix
+  lps[0] = 0; // LPS = Longest Prefix Suffix Array
+
+  // Loop to calculate lps[i] for i = 1 to M-1
+  int i = 1;
+  while (i < M)
+  {
+    if (pat[i] == pat[len])
+    {
+      len++;
+      lps[i] = len;
+      i++;
+    }
+    else // (pat[i] != pat[len])
+    {
+      if (len != 0)
+        len = lps[len - 1];
+      else // if (len == 0)
+      {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+}
+
+#endif // KMP_H
--- Actividad_Integradora/ACT_1/main.cpp
+++ Actividad_Integradora/ACT_1/main.cpp
@@ -1,37 +1,101 @@
-/**
- * @file main.cpp
- * @author Carlos Salguero
- * @author Sergio Garnica
- * @author Alejandra Cabrera
- * @brief Implementation of Actividad Integradora 1
- * @version 0.1
- * @date 2023-10-1
- *
- * @copyright Copyright (c) 2023
- *
- */
-
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <algorithm>
+#include <filesystem>
 
-int main(int argc, char *argv[])
+#include "kmp.h"
+
+namespace fs = std::filesystem;
+
+// Function prototypes
+std::vector<std::filesystem::path> get_filenames(const std::string_view &);
+std::vector<std::string> get_files_content(std::string directory);
+
+int main(int argc, char **argv)
 {
-  std::ifstream inputFile;
+  std::vector<std::string> transmission_files = get_files_content("transmission");
+  std::vector<std::string> mcode_files = get_files_content("mcode");
 
-  if (argc < 2)
-  {
-    std::cout << "Usage: " << argv[0] << " <filename>\n";
-    return -1;
-  }
-
-  inputFile.open(argv[1]);
-  if (!inputFile.is_open())
-  {
-    std::cout << argv[0] << ": File \"" << argv[1] << "\" not found\n";
-    return -1;
-  }
+  for (const auto &file : transmission_files)
+    for (const auto &mcode : mcode_files)
+      KMPSearch(const_cast<char *>(mcode.c_str()), const_cast<char *>(file.c_str()));
 
   return 0;
+}
+
+// Function definitions
+// Auxiliar functions
+/**
+ * @brief
+ * Gets the mcode memory array from the mcode files
+ * @param files_directory - Directory containing the mcode files
+ * @return std::vector<std::string> - Vector of strings containing the mcode memory
+ */
+std::vector<std::string> get_files_content(std::string directory)
+{
+  auto files = get_filenames(directory);
+  std::vector<std::string> file_memory;
+  for (const auto &file : files)
+  {
+    std::string filename = file.filename().string();
+    std::cout << "[ARCHIVO]: " << filename << std::endl;
+
+    std::ifstream read_file(file);
+    if (read_file.is_open())
+    {
+      std::string file_content((std::istreambuf_iterator<char>(read_file)), std::istreambuf_iterator<char>());
+      file_memory.push_back(file_content.c_str());
+
+      // Print file content
+      if (file_content.size() > 20)
+        std::cout << "[CONTENIDO]: \n"
+                  << file_content << "\n\n";
+      else
+        std::cout << "[CONTENIDO]: " << file_content << "\n\n";
+    }
+    else
+    {
+      std::cerr << "Error: No se pudo abrir el archivo " << filename << std::endl;
+      exit(1);
+    }
+    read_file.close();
+  }
+  return file_memory;
+}
+
+/**
+ * @brief
+ * Gets the filename from the arguments passed to the program
+ * @param filename - Filename passed to the program
+ * @return std::vector<std::filesystem::path> - Vector of filenames
+ */
+std::vector<std::filesystem::path> get_filenames(
+    const std::string_view &input_directory)
+{
+  std::vector<std::filesystem::path> filenames;
+  std::filesystem::path path{input_directory};
+
+  if (!std::filesystem::is_directory(path))
+  {
+    std::cerr << "Error: " << input_directory << " is not a valid directory" << std::endl;
+    exit(1);
+  }
+
+  for (const auto &entry : std::filesystem::directory_iterator(path))
+  {
+    if (entry.path().extension() == ".txt")
+    {
+      filenames.push_back(entry.path());
+    }
+  }
+
+  if (filenames.empty())
+  {
+    std::cerr << "Error: no input files found in " << input_directory << std::endl;
+    exit(1);
+  }
+
+  return filenames;
 }
