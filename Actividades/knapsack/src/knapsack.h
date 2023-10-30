e3a6b2b176f2fe0c9a002f9d4d1f270fb76eb1c4
Sergio Garnica <sgarnica1902@gmail.com>
2023-10-29 18:31:34 -0600

    Print matrix

--- Actividades/knapsack/src/knapsack.h
+++ Actividades/knapsack/src/knapsack.h
@@ -3,7 +3,7 @@
  * @brief Dynamic approach to the 0/1 knapsack problem
  * @author Alejandra Cabrera
  * @author Carlos Salguero
- * @author Sergio Garnia
+ * @author Sergio Garnica
  * @version 1.0.0
  * @date 2023-10-23
  *
@@ -31,34 +31,62 @@
 int knapsack(int max_weight, const std::vector<int> &weights,
              const std::vector<int> &values, int num_items)
 {
-    if (weights.size() != values.size())
-        throw std::invalid_argument("Mismatch in sizes of input vectors");
+  if (weights.size() != values.size())
+    throw std::invalid_argument("Mismatch in sizes of input vectors");
 
-    if (weights.size() != num_items)
-        throw std::invalid_argument("Mistach in weights size with num items");
+  if (weights.size() != num_items)
+    throw std::invalid_argument("Mismatch in weights size with num_items");
 
-    for (int i{}; i < num_items; ++i)
+  for (int i{}; i < num_items; ++i)
+  {
+    if (weights[i] < 0)
+      throw std::invalid_argument("Negative weights are not allowed");
+
+    else if (values[i] < 0)
+      throw std::invalid_argument("Negative values are not allowed");
+  }
+
+  if (max_weight < 0)
+    throw std::invalid_argument("Negative max weight is not allowed");
+
+  std::vector<std::vector<int>> dp(num_items + 1, std::vector<int>(max_weight + 1, 0));
+
+  for (int index{}; index <= num_items; ++index)
+  {
+    for (int weight = 0; weight <= max_weight; ++weight)
     {
-        if (weights[i] < 0)
-            throw std::invalid_argument(
-                "Negative values are not allowed");
-
-        else if (values[i] < 0)
-            throw std::invalid_argument(
-                "Negative weights are not allowed");
+      if (index == 0 || weight == 0)
+        dp[index][weight] = 0;
+      else if (weights[index - 1] <= weight)
+        dp[index][weight] = std::max(dp[index - 1][weight], values[index - 1] + dp[index - 1][weight - weights[index - 1]]);
+      else
+        dp[index][weight] = dp[index - 1][weight];
     }
+  }
 
-    if (max_weight < 0)
-        throw std::invalid_argument("Negative max weight is not allowed");
+  // print values
+  std::cout << "Values: ";
+  for (int i = 0; i < num_items; ++i)
+  {
+    std::cout << values[i] << " ";
+  }
+  std::cout << std::endl;
 
-    std::vector<int> dp(max_weight + 1, 0);
+  // Print the dynamic programming matrix
+  for (int i = 0; i < num_items; ++i)
+  {
+    std::cout << values[i] << ": ";
+    for (int j = 0; j <= max_weight; ++j)
+    {
+      std::cout << dp[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
 
-    for (int index{}; index < num_items; ++index)
-        for (int weight = max_weight; weight >= weights[index]; --weight)
-            dp[weight] = std::max(dp[weight],
-                                  values[index] + dp[weight - weights[index]]);
+  // Print result
+  std::cout << "\nOptimal profit: " << dp[num_items][max_weight] << "\n\n";
 
-    return dp[max_weight];
+  return dp[num_items][max_weight];
 }
 
 #endif //! KNAPSACK_H
--- Actividades/knapsack/src/main.cpp
+++ Actividades/knapsack/src/main.cpp
@@ -3,7 +3,7 @@
  * @brief Entry point of the program
  * @author Alejandra Cabrera
  * @author Carlos Salguero
- * @author Sergio Garnia
+ * @author Sergio Garnica
  * @version 1.0.0
  * @date 2023-10-23
  *
@@ -33,30 +33,34 @@ const std::string RESET = "\033[0m";
 void test_knapsack(int test_num, int N, const std::vector<int> &values,
                    const std::vector<int> &weights, int W, int expected)
 {
-    int result{knapsack(W, weights, values, N)};
+  std::cout << "== Test " << test_num << " =="
+            << "\n\n";
+  int result{knapsack(W, weights, values, N)};
 
-    if (result == expected)
-        std::cout << "Test " << test_num << ": "
-                  << GREEN << " PASSED " << RESET << std::endl;
+  if (result == expected)
+    std::cout << "Test " << test_num << ": "
+              << GREEN << " PASSED " << RESET << std::endl;
 
-    else
-        std::cout << "Test " << test_num << ": "
-                  << RED << " FAILED " << RESET
-                  << " (Expected: " << expected << ", Got: " << result << ")"
-                  << std::endl;
+  else
+    std::cout << "Test " << test_num << ": "
+              << RED << " FAILED " << RESET
+              << " (Expected: " << expected << ", Got: " << result << ")"
+              << std::endl;
+
+  std::cout << "\n====================\n\n";
 }
 
 int main()
 {
-    test_knapsack(1, 3, {1, 2, 3},
-                  {4, 5, 1}, 4, 3);
+  test_knapsack(1, 3, {1, 2, 3},
+                {4, 5, 1}, 4, 3);
 
-    test_knapsack(2, 4, {10, 40, 30, 50},
-                  {5, 4, 6, 3}, 10, 90);
+  test_knapsack(2, 4, {10, 40, 30, 50},
+                {5, 4, 6, 3}, 10, 90);
 
-    test_knapsack(3, 5, {60, 100, 120, 130, 75},
-                  {10, 20, 30, 25, 15}, 50, 265);
+  test_knapsack(3, 5, {60, 100, 120, 130, 75},
+                {10, 20, 30, 25, 15}, 50, 265);
 
-    test_knapsack(4, 3, {30, 14, 16},
-                  {6, 3, 4}, 10, 46);
+  test_knapsack(4, 3, {30, 14, 16},
+                {6, 3, 4}, 10, 46);
 }
